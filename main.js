eaaeb9ae8efc37c02afc0b21fa220aa717f1374d
MarcosFajoli <marcos.almeida83@etec.sp.gov.br>
2021-12-05 14:15:34 -0300

    feat: add animation

--- main.js
+++ main.js
@@ -14,19 +14,35 @@ function getRequest() {
   request.send()
 }
 
-function setRegisterFormVisibleOrInvisible(state, setter = true) {
+const registerBtn = document.getElementById('registerBtn')
+const listBtn = document.getElementById('listBtn')
+const page1 = document.getElementById('page1')
+const page2 = document.getElementById('page2')
+registerBtn.addEventListener('click', () => {
+  page1.classList.toggle('animation')
+  if (page2.classList.contains('animation'))
+    page2.classList.toggle('animation')
+})
+
+listBtn.addEventListener('click', () => {
+  page2.classList.toggle('animation')
+  if (page1.classList.contains('animation')) 
+    page1.classList.toggle('animation')
+})
+
+function setPage1VisibleOrInvisible(state, setter = true) {
   if (setter) 
-    setRegisteredListVisibleOrInvisible('hidden', false)
-  const registerForm = document.getElementById('registerForm')
-  const { style } = registerForm
+    setPage2VisibleOrInvisible('hidden', false)
+  const page1 = document.getElementById('page1')
+  const { style } = page1
   style.visibility = state
 }
 
-function setRegisteredListVisibleOrInvisible(state, setter = true) {
+function setPage2VisibleOrInvisible(state, setter = true) {
   if (setter) 
-    setRegisterFormVisibleOrInvisible('hidden', false)
-  const registeredList = document.getElementById('registeredList')
-  const { style } = registeredList
+    setPage1VisibleOrInvisible('hidden', false)
+  const page2 = document.getElementById('page2')
+  const { style } = page2
   style.visibility = state
 }
 
